8817d100dbbfe9d89e988fed1a39ec6ed2ee754d
jlostaln <136805206+jlostaln@users.noreply.github.com>
2025-07-30 09:30:18 +0300

    refactoring: text_to_html files combined with textnode files

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -38,6 +38,45 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
         self.assertEqual("TextNode(This is a text node, bold, https://www.boot.dev)", repr(node)
         )
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+
+    def test_text_type_not_in_enum(self):
+        node = TextNode("This is a text node", "boldanditalic")
+
+        with self.assertRaises(ValueError) as context:
+            text_node_to_html_node(node)
+        self.assertEqual(
+            str(context.exception),
+            "Invalid textnode.text_type: text_type 'boldanditalic' not a member of TextType enum!"
+        )
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("Hello world!", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "Hello world!")
+
+    def test_link(self):
+        node = TextNode("Click here!", TextType.LINK, "https://www.google.com" )
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Click here!")
+        self.assertEqual(html_node.props, {"href": "https://www.google.com"})
+
+    def test_image(self):
+        node = TextNode("alt text", TextType.IMAGE, "/link/to/image")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "/link/to/image", "alt": "alt text"})
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+from htmlnode import LeafNode
 from enum import Enum
 
 class TextType(Enum):
@@ -23,4 +24,30 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-            
+
+
+def text_node_to_html_node(textnode: TextNode):
+    if not isinstance(textnode.text_type, TextType):
+        raise ValueError(f"Invalid textnode.text_type: text_type '{textnode.text_type}' not a member of TextType enum!")
+
+    props = None
+    text = textnode.text
+    match textnode.text_type:
+        case TextType.TEXT:
+            tag = None
+        case TextType.BOLD:
+            tag = "b"
+        case TextType.ITALIC:
+            tag = "i"
+        case TextType.CODE:
+            tag = "code"
+        case TextType.LINK:
+            tag = "a"
+            props = {"href": textnode.url} 
+        case TextType.IMAGE:
+            tag = "img"
+            props = {"src": textnode.url, "alt": text}
+            text = ""
+
+    return LeafNode(tag, text, props)
+
