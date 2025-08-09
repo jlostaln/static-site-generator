3ded8cbd8f69a9c50e0ee64f4088235846321d7a
jlostaln <136805206+jlostaln@users.noreply.github.com>
2025-08-09 07:21:53 +0300

    build site in docs/ for Github Pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -9,7 +9,7 @@ def extract_title(markdown):
     raise ValueError("Invalid markdown: No h1 header found!")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as f:
         content = f.read()
@@ -19,19 +19,20 @@ def generate_page(from_path, template_path, dest_path):
     html_string = html_nodes.to_html()
     title = extract_title(content)
     html_output = template.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+    html_output = html_output.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
 
     parent_dir = os.path.dirname(dest_path)
     os.makedirs(parent_dir, exist_ok=True)
     with open(dest_path, "w") as f:
         f.write(html_output)
 
-def generate_page_recursively(from_path, template_path, dest_path):
+def generate_page_recursively(from_path, template_path, dest_path, basepath):
     for item in os.listdir(from_path):
         source_path = os.path.join(from_path, item) 
         destination_path = os.path.join(dest_path, item).replace(".md", ".html") 
 
         if os.path.isdir(source_path):
-            generate_page_recursively(source_path, template_path, destination_path)
+            generate_page_recursively(source_path, template_path, destination_path, basepath)
         else:
-            generate_page(source_path, template_path, destination_path)
+            generate_page(source_path, template_path, destination_path, basepath)
 
--- src/main.py
+++ src/main.py
@@ -1,18 +1,21 @@
+import sys
 from textnode import TextNode, TextType
 from copy_static import copy_static_to_public
 from generate_page import generate_page_recursively
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 from_path = "./content"
 template_path = "./template.html"
-dest_path = "./public"
+dest_path = "./docs"
 
 def main():
 
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
     copy_static_to_public(dir_path_static, dir_path_public)
-    generate_page_recursively(from_path, template_path, dest_path)
-
+    generate_page_recursively(from_path, template_path, dest_path, basepath)
 
 
 if __name__ == "__main__":
